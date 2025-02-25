3ee4c3021e43f3f801ede7f58090ee46b34e137a
Kubra Konakci <kbrakonakci@gmail.com>
2025-02-25 16:51:45 +0300

    dropdown oluşturuldu

--- src/App.jsx
+++ src/App.jsx
@@ -27,8 +27,10 @@ function App() {
       return acc;
     }, {})
   );
-
-  const [message, setMessage] = useState('');
+  const [secim, setSecim] = useState('');//radiobutton
+  const [message, setMessage] = useState('');//pizza malzemeleri
+  const [hamur, setHamur] = useState('');
+  
 
   // Checkbox durumunu değiştiren fonksiyon
   const handleCheckboxChange = (event) => {
@@ -50,12 +52,23 @@ function App() {
       ...prevToppings,
       [name]: checked,
     }));
+  }
+  //radio button
+  const boyutlar = ['Küçük', 'Orta', 'Büyük'];  // Seçenekler
+
+  const handleChange = (event) => {
+    setSecim(event.target.value);  // Seçilen değeri state'e kaydet
   };
+
+  const handleHamurChange = (event) => {
+    setHamur(event.target.value);  // Seçilen değeri state'e kaydet
+  };
+    
 
   return (
     <>
       <header className='header'>
-        <h1>Teknolojik Yemekler</h1>
+        <img src="../images/iteration-1-images/logo.svg" alt="" />
         <nav className='navigation'>
           <a href="">Anasayfa-</a>
           <a href="">Seçenekler-</a>
@@ -65,10 +78,41 @@ function App() {
       <div className='center'>
         <h3>Position Absolute Acı Pizza</h3>
         <h2>85.50TL</h2>
-        <p>asdfojpdofıjpdsfoıjdpsfıjsapdfıjpasdfıjfpfjpsdofjpfosjfdapsfdpfıp
-          sdoıjpodsjofpıdjspodfjpsdfjpodfıj
-          sadaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        <p>Frontend developer olarak hala position:absolute kullanıyorsan bu çok acı pizza tam sana göre.
         </p>
+       
+      <div className='button-select'>
+         <section>
+          <h3>Boyut Seç</h3>
+         <form>
+           {boyutlar.map((boyut, index) => (
+           <label key={index} className="radio-label">
+            <input
+              type="radio"
+              name="boyut"
+              value={boyut.toLowerCase()}
+              onChange={handleChange}
+            />
+            {boyut}
+          </label>
+        ))}
+      </form>
+      </section>
+      <section>
+      <h3>Hamur Seç</h3>
+      <form>
+        <label htmlFor="hamur">Hamur Kalınlığı:</label>
+        <select id="hamur" name="hamur" onChange={handleHamurChange} value={hamur}>
+          <option value="ince">İnce</option>
+          <option value="orta">Orta</option>
+          <option value="kalin">Kalın</option>
+        </select>
+      </form>
+      </section>
+      
+    </div>
+
+   
         <h3>Ek Malzemeler</h3>
         <p>En fazla on malzeme seçebilirsiniz. 5TL</p>
         <form className='form'>
